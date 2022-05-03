05b29786535268ecac9f4269d0ae9d0a9e0932f8
Shabagu <Shabagu@yandex.ru>
2022-05-03 18:57:18 +0300

    minor changes

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -106,9 +106,9 @@ const UserInfo = ({ user }) => {
         <div className={style.subcontainer}>
           <div className={style.picture_box} onClick={popup}>
             <img
-              width='200'
-              src='https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
-              alt=''
+              width="200"
+              src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+              alt=""
               onClick={popup}
             />
           </div>
