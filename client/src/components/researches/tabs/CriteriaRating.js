d29195d77209189851ede5a3dee8ef83fcc5512e
Shabagu <Shabagu@yandex.ru>
2022-05-06 01:21:52 +0300

    research card criteria rating view popup

--- client/src/components/researches/tabs/CriteriaRating.js
+++ client/src/components/researches/tabs/CriteriaRating.js
@@ -1,6 +1,18 @@
+import { useState } from 'react'
+
 import style from './CriteriaRating.module.scss'
 
 export const CriteriaRating = ({ research }) => {
+
+  const [popupActive, setPopupActive] = useState(false)
+  const [popupRow, setPopupRow] = useState(0)
+  const [popupCol, setPopupCol] = useState(0)
+
+  const popup = (row, col) => {
+    setPopupActive(true)
+    setPopupRow(row)
+    setPopupCol(col)
+  }
 
   return(
     <div className={style.criteria_rating_container}>
@@ -21,15 +33,23 @@ export const CriteriaRating = ({ research }) => {
               key={i}
               i={i}
               research={research}
+              popup={popup}
             />
           )}
         </tbody>
       </table>
+      <RatingViewPopup
+        active={popupActive}
+        setActive={setPopupActive}
+        research={research}
+        row={popupRow}
+        col={popupCol}
+      />
     </div>
   )
 }
 
-const CellRow = ({ i, research }) => {
+const CellRow = ({ i, research, popup }) => {
   return(
     <tr>
       <th title={research.criteria[i]}>
@@ -37,12 +57,12 @@ const CellRow = ({ i, research }) => {
       </th>
       {[...Array(research.criteria.length)].map((x, j) => {
         if (i < j) return (
-          <td key={j} className={style.top}>
+          <td key={j} className={style.top} onClick={ () => popup(i, j)}>
             {MARK_MODEL[research.criteriaRating[i][j]].string}
           </td>
         )
         else if (i > j) return (
-          <td key={j} className={style.bot}>
+          <td key={j} className={style.bot} onClick={ () => popup(i, j)}>
             {MARK_MODEL[research.criteriaRating[i][j]].string}
           </td>
         )
@@ -55,6 +75,51 @@ const CellRow = ({ i, research }) => {
     </tr>
   )
 }
+
+
+
+
+const RatingViewPopup = ({
+  active,
+  setActive,
+  research,
+  row,
+  col,
+}) => {
+
+  const confirmHandler = () => { setActive(false) }
+
+  return(
+    <div
+      className={active ? `${style.popup} ${style.active}` : style.popup}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={active ? `${style.popup_content} ${style.active}` : style.popup_content}
+        onClick={e => e.stopPropagation()}
+      >
+        <p className="center">Сравнение критериев</p>
+        <div className="center">{MARK_MODEL[research.criteriaRating[row][col]].string}</div>
+        <div className={style.rating_box}>
+          <div>{research.criteria[col]}</div>
+          <div>{research.criteria[row]}</div>
+        </div>
+        <div className="range-field">
+          <input
+            type="range" id="criteria" name="criteria"
+            min={0} max={16} value={[research.criteriaRating[row][col]]} readOnly={true} tabIndex={-1}
+          />
+        </div>
+        <div className={style.popup_exit}>
+          <i className="material-icons" onClick={confirmHandler}>close</i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+
 
 // Модель оценок 
 class Mark {
